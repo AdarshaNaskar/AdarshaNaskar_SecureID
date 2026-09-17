9f1045afb957e7ae3d2151ea169aa7db0e307ca2
Adarsha Naskar <adarsha692006@gmail.com>
2026-09-17 21:26:15 +0530

    Fix production database connection

--- backend/database.js
+++ backend/database.js
@@ -6,7 +6,7 @@ if (!process.env.DATABASE_URL) {
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  max: 10,
+  max: 5,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 15000,
 });
